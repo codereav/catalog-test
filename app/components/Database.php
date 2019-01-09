951325887520fb1c7df88d9a5e2ffaae94af1c59
codereav <efremovav91@gmail.com>
2019-01-09 13:41:11 +0400

    FIX: mysql encoding

--- app/components/Database.php
+++ app/components/Database.php
@@ -24,7 +24,7 @@ class Database
             $dbConfig['password'],
             [
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8 COLLATE ' . $dbConfig['collation']
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE ' . $dbConfig['collation']
             ]
         );
         if ($db) {
